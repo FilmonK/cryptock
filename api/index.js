c6f1d1fad076879f09fc16456b2002dfe97e6235
Filmon Kesete <filmonkesete@Filmons-Laptop.local>
2023-05-04 10:04:15 -0500

    adding routes to backend to test getting data from alpaca API to send to front.

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const {google} = require('googleapis');
-const fs = require('fs');
-const readline = require('readline');
-const { auth } = require('google-auth-library');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const fs = require('fs');
+const WebSocket = require('ws');
+const Alpaca = require("@alpacahq/alpaca-trade-api");
+const alpaca = new Alpaca({
+    keyId: process.env.APCA_API_KEY_ID,
+    secretKey: process.env.APCA_API_SECRET_KEY,
+    paper: true,
+  });
 
 app.use(express.urlencoded({
     extended: true
@@ -17,62 +21,22 @@ app.use(cors())
 
 require('dotenv').config()
 
-//route to get trades from google sheet
-app.get('/trades', async (req, res) => {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: process.env.KEYFILE,
-        scopes: 'https://www.googleapis.com/auth/spreadsheets'
-    })
-
-
-    //create a new client 
-    const client = await auth.getClient();
-    const googleSheets = await google.sheets({
-        version: 'v4', 
-        auth: client
-    })
-    const spreadsheetId = process.env.SPREADSHEET_ID
-
-    //testing getting meta data
-    const getMetaData = await googleSheets.spreadsheets.get({
-        auth,
-        spreadsheetId, 
-    });
-
-    //testing receiving data from sheet
-    const getSSData = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId,
-        range: 'Sheet1!A:H'
-    })
+// route to get positions from alpaca
+app.get('/positions', async (req, res) => {
+    const closedOrders = await alpaca
+        .getOrders({
+            status: "closed",
+            limit: 100,
+            nested: true, // show nested multi-leg orders
+        })
+        .then((data) => {
+            console.log(data)
+            res.send(data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
     
-    // res.send(getMetaData);
-    res.send(getSSData);
-
-})
-
-
-//route to get ownership percentages
-app.get('/all', async(req, res) => {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: process.env.KEYFILE,
-        scopes: 'https://www.googleapis.com/auth/spreadsheets'
-    })
-
-    const client = await auth.getClient();
-    const googleSheets = await google.sheets({
-        version: 'v4',
-        auth: client
-    })
-
-    const spreadsheetId = process.env.SPREADSHEET_ID
-    const getPercData = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId,
-        range: 'Sheet2!A:B'
-    })
-
-    res.send(getPercData)
 })
 
 
